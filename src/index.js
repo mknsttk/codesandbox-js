7fffc34e28460e7445fe924e1a993f18f29ff0a6
mknsttk <65141237+mknsttk@users.noreply.github.com>
2020-11-25 11:00:35 +0900

    テキストの追加機能を追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得
+  const inputText = document.getElementById("input-text").value;
+  document.getElementById("input-text").value = "";
+
+  // divを生成
+  const div = document.createElement("div");
+  div.className = "list-item";
+
+  //liを生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divの子要素にliを入れる
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document.getElementById("add").addEventListener("click", () => {
+  onClickAdd();
+});
